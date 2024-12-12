aa78c02c370dc779e48a19a89c5048d2d1226e53
leafaraujo <rafael.araujo.and@gmail.com>
2024-12-12 16:33:13 -0300

    Estou colocando o repositories no repositório correto

---
+++ demo/src/main/java/com/example/demo/repositories/FuncionarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {  
+}
